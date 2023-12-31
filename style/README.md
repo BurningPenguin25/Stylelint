8deb5e8616d0d4a9aae262a227bc489e2fd129a9
Pavel Bodnar <pasha@codeas.ru>
2023-12-31 18:20:56 +0200

    Stylelint first commit

---
+++ style/README.md
@@ -0,0 +1,12 @@
+ближайшиее версии до 17 октября 
+
+stylelint@15.11.0
+stylelint-config-standart-scss@11.0.0
+stylelint-order@6.0.3
+stylelint-scss@5.2.1
+stylelint-color-format       - без версии 
+
+
+++
+postcss-scss
+postcss-html
---
+++ style/src/app/page.js
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import styles from './page.module.scss'
+
+export default function Home() {
+  return (
+      <div className="App">
+        <div className="purple">
+          <p>Purple</p>
+        </div>
+        <div className="divider"></div>
+        <div className="black">
+          <p>Black</p>
+        </div>
+      </div>
+  )
+}
